65b5dff9625ed14263e26bdff8883d1c657e3da6
PAAPCA02LC200 <PAAPCA02LC200@PAAPCA02LC200.duoc.acad>
2025-06-18 22:20:02 -0400

    avancs programacion

--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/controller/CarreraController.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/controller/CarreraController.java
@@ -1,12 +1,14 @@
 package biblioteca.salas.duoc.biblioteca.salas.duoc.controller;
 
 import biblioteca.salas.duoc.biblioteca.salas.duoc.service.CarreraService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/carrera") // <-- ruta base para mis servicios de carreras
+@Tag(name = "Servicios de Carreras", description = "Servicios para poder consultar informacion de las carreras disponibles en la plataforma")
 public class CarreraController {
 
     @Autowired
